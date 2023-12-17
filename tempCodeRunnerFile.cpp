cb2ba64e3e0677e349f75e7128b023f3825cbb8d
Harish Kumar <harishk820@gmail.com>
2023-12-17 12:53:24 +0530

    temp file config

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int sum(int a, int b)
+{
+  return a + b;
+}
+
 int main()
 {
 
   cout << "hi Ubuntu" << endl;
   cout << "mera code mast chal raha ah ab" << endl;
   cout << "ubuntu desktop setup" << endl;
+  // sum function call
+
+  int sumRes = sum(30, 77);
+  cout << sumRes << endl;
+
   return 0;
 }
