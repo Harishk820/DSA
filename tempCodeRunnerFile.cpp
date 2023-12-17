78f465e47975c18dab8ecb13314738b9a99098c3
Harish Kumar <harishk820@gmail.com>
2023-12-17 12:44:56 +0530

    ubuntu desktop branch changes

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -5,6 +5,7 @@ int main()
 {
 
   cout << "hi Ubuntu" << endl;
-  cout << "mera code mast chal raha ah ab";
+  cout << "mera code mast chal raha ah ab" << endl;
+  cout << "ubuntu desktop setup" << endl;
   return 0;
 }
